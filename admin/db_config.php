e50997345dfae5c3b511ce0703b419a027a978db
berru-g <g.leberruyer@gmail.com>
2025-10-16 12:49:51 +0200

    odd

--- admin/db_config.php
+++ admin/db_config.php
@@ -3,7 +3,7 @@ return [
     'host' => 'localhost',
     'db'   => 'u667977963_golden_dessert',
     'user' => 'u667977963_berru_nico',
-    'pass' => 'm@bddSQL25',
+    'pass' => 'root',
     'charset' => 'utf8mb4'
 ];
 
